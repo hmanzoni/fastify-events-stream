6862311e5df6480bcae520a04a29daf357473c02
Hugo Manzoni <hugocmdesign@gmail.com>
2025-03-31 15:41:51 +0200

    update auth controller with the pg services

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,9 +1,13 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import kaftaProducer from "../services/producer.service.ts";
+import { hashPassword } from "../utils/hash.ts";
+import { loginUser, fetchUser } from "../services/users.service.ts";
 
 // GET	/auth/me	Obtener perfil del usuario autenticado
 export async function getProfile(request: FastifyRequest, reply: FastifyReply) {
-  return reply.send({ hello: "world, from /auth/me" });
+  const username = 'Hugo';
+  const userInfo = await fetchUser(username);
+  return reply.status(200).send({ email: `Your email is: ${userInfo?.email}` });
 }
 
 // POST	/auth/register	Registrar un usuario
@@ -13,8 +17,9 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     password: string;
     email: string;
   };
-  kaftaProducer({ username, password, email });
-  return reply.send({
+  const password_hash = await hashPassword(password);
+  await kaftaProducer({ username, password_hash, email });
+  return reply.status(201).send({
     message: `User ${username} has been registered successfully`,
   });
 }
@@ -25,5 +30,8 @@ export async function login(request: FastifyRequest, reply: FastifyReply) {
     username: string;
     password: string;
   };
-  return reply.send({ hello: "world, from /auth/login" });
+  const userInfo = await loginUser({ username, password });
+  return reply.status(200).send({
+    message: `Hello ${userInfo.username} ! You have logged in successfully`,
+  });
 }
