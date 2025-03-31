78dce35148063046fd7185c7975bad9e610a9858
Hugo Manzoni <hugocmdesign@gmail.com>
2025-03-31 11:49:20 +0200

    add prisma schema and client

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "event_logs" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "event_type" VARCHAR(50) NOT NULL,
+    "user_id" INTEGER,
+    "timestamp" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "metadata" JSONB,
+
+    CONSTRAINT "event_logs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "event_logs" ADD CONSTRAINT "event_logs_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
