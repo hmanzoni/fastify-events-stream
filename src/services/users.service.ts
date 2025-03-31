d27ac2898cb876d7ba84a131aa42b55939150896
Hugo Manzoni <hugocmdesign@gmail.com>
2025-03-31 15:33:50 +0200

    add users services, prisma logic for handle the db Postgres

---
+++ src/services/users.service.ts
@@ -0,0 +1,32 @@
+import prisma from "../utils/prisma.ts";
+import { comparePassword } from "../utils/hash.ts";
+
+export async function fetchUser(username: string) {
+  const user = await prisma.users.findUnique({
+    where: { username },
+  });
+  return user;
+}
+
+export async function registerUser(data: { username: string; password_hash: string; email: string }) {
+  const { username, password_hash, email } = data;
+  const userInfo = await fetchUser(username);
+  if (userInfo) {
+    throw new Error("User already exists");
+  }
+  return await prisma.users.create({
+    data: { username, email, password_hash },
+  });
+}
+
+export async function loginUser(data: { username: string; password: string; }) {
+  const userInfo = await fetchUser(data.username);
+  if (!userInfo) {
+    throw new Error("User not found");
+  }
+  const passMatch = await comparePassword(data.password, userInfo.password_hash);
+  if (!passMatch) {
+    throw new Error("Login failed");
+  }
+  return userInfo;
+}
