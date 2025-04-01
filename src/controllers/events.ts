aed877b12f2b6f6fb8106282a33e44c38b276958
Hugo Manzoni <hugocmdesign@gmail.com>
2025-04-01 10:44:18 +0200

    add controller logic

--- src/controllers/events.ts
+++ src/controllers/events.ts
@@ -1,11 +1,9 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import kaftaProducer from "../services/producer.service.ts";
+import { getTopEvents, getEventById } from "../services/eventsCh.service.ts";
 
 // POST	/events	Recibir eventos y enviarlos a Kafka
-export async function handleEvents(
-  request: FastifyRequest,
-  reply: FastifyReply
-) {
+export async function handleEvents( request: FastifyRequest, reply: FastifyReply ) {
   const { eventType, userId, metadata } = request.body as {
     eventType: string;
     userId: string;
@@ -17,11 +15,16 @@ export async function handleEvents(
 
 // GET	/events/recent	Obtener los últimos eventos registrados
 export async function getRecents(request: FastifyRequest, reply: FastifyReply) {
-  return reply.send({ hello: "world, from /events/recent" });
+  let limitEvents;
+  const { topEvents } = request.query as { topEvents: string | undefined };
+  limitEvents = topEvents || "10";
+  const rows = await getTopEvents(limitEvents);
+  return reply.send({ topEvents: limitEvents, rows });
 }
 
 // GET	/events/:id	Obtener un evento específico
 export async function getEvent(request: FastifyRequest, reply: FastifyReply) {
   const { id } = request.params as { id: string };
-  return reply.send({ hello: `world, from /events/${id}` });
+  const rows = await getEventById(id);
+  return reply.send({ eventId: id, rows: rows });
 }
