fe566a6748d9d2f6020fe420ac851cdced3a9779
Hugo Manzoni <hugocmdesign@gmail.com>
2025-04-02 15:53:45 +0200

    create the services for logs on pg

---
+++ src/services/logs.service.ts
@@ -0,0 +1,19 @@
+import prisma from "../utils/prisma.ts";
+
+export async function fetchLogs() {
+  const logs = await prisma.event_logs.findMany({
+    orderBy: { timestamp: "desc" }
+  });
+  return logs;
+}
+
+export async function saveLogs(data: { event_type: string; user_id: string; metadata: any }) {
+  const { event_type, user_id, metadata } = data;
+  return await prisma.event_logs.create({
+    data: {
+      event_type,
+      user_id,
+      metadata,
+    },
+  });
+}
