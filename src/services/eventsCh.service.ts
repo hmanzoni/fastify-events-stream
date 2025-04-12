5f51c81ff2f785bc6639dc3f9fefba6d8e4f3317
Hugo Manzoni <hugocmdesign@gmail.com>
2025-04-13 01:33:02 +0200

    remove repets

--- src/services/eventsCh.service.ts
+++ src/services/eventsCh.service.ts
@@ -2,10 +2,11 @@ import { type InsertParams, type QueryParams } from "@clickhouse/client";
 import clientCH from "../utils/chouse.util.js";
 import Stream from "stream";
 
+const queryDefaultCols: string = "COLUMNS('event_id'), COLUMNS('user_id'), COLUMNS('event_type'), COLUMNS('timestamp')";
+
 export const getTopEvents = async (limit: string = "10") => {
-  const queryCols: string = "COLUMNS('event_id'), COLUMNS('user_id'), COLUMNS('event_type'), COLUMNS('timestamp')";
   const queryCh: QueryParams = {
-    query: `SELECT ${queryCols} FROM events LIMIT ${limit}`,
+    query: `SELECT ${queryDefaultCols} FROM events LIMIT ${limit}`,
     format: "JSONEachRow",
   };
   const rows = await clientCH.query(queryCh);
@@ -13,9 +14,8 @@ export const getTopEvents = async (limit: string = "10") => {
 };
 
 export const getAllUsersEvents = async () => {
-  const queryCols: string = "COLUMNS('event_id'), COLUMNS('user_id'), COLUMNS('event_type'), COLUMNS('timestamp')";
   const queryCh: QueryParams = {
-    query: `SELECT ${queryCols} FROM events`,
+    query: `SELECT ${queryDefaultCols} FROM events`,
     format: "JSONEachRow",
   };
   const rows = await clientCH.query(queryCh);
@@ -23,9 +23,8 @@ export const getAllUsersEvents = async () => {
 };
 
 export const getEventById = async (id: string) => {
-  const queryCols: string = "COLUMNS('event_id'), COLUMNS('user_id'), COLUMNS('event_type'), COLUMNS('timestamp')";
   const queryCh: QueryParams = {
-    query: `SELECT ${queryCols} FROM events WHERE event_id = '${id}'`,
+    query: `SELECT ${queryDefaultCols} FROM events WHERE event_id = '${id}'`,
     format: "JSONEachRow",
   };
   const rows = await clientCH.query(queryCh);
@@ -33,9 +32,8 @@ export const getEventById = async (id: string) => {
 };
 
 export const getEventByUserId = async (id: string) => {
-  const queryCols: string = "COLUMNS('event_id'), COLUMNS('user_id'), COLUMNS('event_type'), COLUMNS('timestamp')";
   const queryCh: QueryParams = {
-    query: `SELECT ${queryCols} FROM events WHERE user_id = '${id}'`,
+    query: `SELECT ${queryDefaultCols} FROM events WHERE user_id = '${id}'`,
     format: "JSONEachRow",
   };
   const rows = await clientCH.query(queryCh);
