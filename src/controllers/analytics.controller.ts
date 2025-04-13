9b2970c629200a1b57713ed5e4f5c388cc6e065f
Hugo Manzoni <hugocmdesign@gmail.com>
2025-04-14 00:28:16 +0200

    refactor analytics

--- src/controllers/analytics.controller.ts
+++ src/controllers/analytics.controller.ts
@@ -1,22 +1,50 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import { getTopEvents, getEventByUserId } from "../services/eventsCh.service.js";
+import { DataLoggerKafka, KafkaLoggerProducer } from "../services/logger.service.js";
+import { EventsEnumType } from "../models/event.model.js";
 
-type TopEventsQuery = { topEvents: string | undefined }
+type TopEventsQuery = { topEvents: string | undefined };
 
 // GET	/analytics/top-events	Get the most frequent events
-export const events = async (request: FastifyRequest, reply: FastifyReply) =>{
+export const events = async (request: FastifyRequest, reply: FastifyReply) => {
   let limitEvents: string;
   const { topEvents } = request.query as TopEventsQuery;
   limitEvents = topEvents || "10";
-  const rows = await getTopEvents(limitEvents);
-  return reply.status(200).send({ topEvents: limitEvents, rows });
-}
+  const dataKafkaProducer: DataLoggerKafka = {
+    request,
+    userId: request.user.userId,
+    serviceName: "analytics-top-events",
+    actionType: EventsEnumType.topEvents,
+    isSuccess: true,
+  };
+  try {
+    const rows = await getTopEvents(limitEvents);
+    await KafkaLoggerProducer(dataKafkaProducer);
+    return reply.status(200).send({ topEvents: limitEvents, rows });
+  } catch (error) {
+    await KafkaLoggerProducer({ ...dataKafkaProducer, isSuccess: false });
+    return reply.status(500).send({ message: error });
+  }
+};
 
-type UserIdParams = { id: string }
+type UserIdParams = { id: string };
 
 // GET	/analytics/user/:id	Get the events from a specific user
-export const user = async (request: FastifyRequest, reply: FastifyReply) =>{
+export const user = async (request: FastifyRequest, reply: FastifyReply) => {
   const { id } = request.params as UserIdParams;
-  const rows = await getEventByUserId(id);
-  return reply.status(200).send({ eventId: id, rows: rows });
-}
+  const dataKafkaProducer: DataLoggerKafka = {
+    request,
+    userId: request.user.userId,
+    serviceName: "analytics-user-events",
+    actionType: EventsEnumType.analyticsUser,
+    isSuccess: true,
+  };
+  try {
+    const rows = await getEventByUserId(id);
+    await KafkaLoggerProducer(dataKafkaProducer);
+    return reply.status(200).send({ eventId: id, rows: rows });
+  } catch (error) {
+    await KafkaLoggerProducer({ ...dataKafkaProducer, isSuccess: false });
+    return reply.status(500).send({ message: error });
+  }
+};
