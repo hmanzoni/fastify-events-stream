dda14f722c89dfd22c7712b254e4ce639bd2234b
Hugo Manzoni <hugocmdesign@gmail.com>
2025-04-14 12:26:51 +0200

    add middleware for protect routes

--- src/routes/v1/analytics.route.ts
+++ src/routes/v1/analytics.route.ts
@@ -1,12 +1,13 @@
 import type { FastifyInstance } from "fastify";
 import { events, user } from './../../controllers/analytics.controller.js';
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
 
 const routes = async (fastify: FastifyInstance)  => {
   // GET	/analytics/top-events	Get the most frequent events
-  fastify.get("/top-events", events);
+  fastify.get("/top-events", { preHandler: [authMiddleware] }, events);
 
   // GET	/analytics/user/:id	Get the events from a specific user
-  fastify.get("/user/:id", user);
+  fastify.get("/user/:id", { preHandler: [authMiddleware] }, user);
 }
 
 export default routes;
--- src/routes/v1/events.route.ts
+++ src/routes/v1/events.route.ts
@@ -1,15 +1,16 @@
 import type { FastifyInstance } from "fastify";
 import { handleEvents, getRecents, getEvent } from "../../controllers/events.controller.js";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
 
 const routes = async (fastify: FastifyInstance) => {
   // POST	/events	send logs events to Kafka
-  fastify.post("/", handleEvents);
+  fastify.post("/", { preHandler: [authMiddleware] }, handleEvents);
 
   // GET	/events/recent	Get the last events registered
-  fastify.get("/recent", getRecents);
+  fastify.get("/recent", { preHandler: [authMiddleware] }, getRecents);
 
   // GET	/events/:id get a specific event
-  fastify.get("/:id", getEvent);
+  fastify.get("/:id", { preHandler: [authMiddleware] }, getEvent);
 }
 
 export default routes;
