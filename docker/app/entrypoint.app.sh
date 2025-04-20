59c9a1ff7a8e32b1b857a674819a6de4effb88e8
Hugo Manzoni <hugocmdesign@gmail.com>
2025-04-20 09:46:49 +0200

    create dockerfile for run app and consumer

---
+++ docker/app/entrypoint.app.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "⏳ Waiting for PostgreSQL..."
+bash /wait-for-it.sh $POSTGRES_HOST:$POSTGRES_PORT --timeout=30 --strict -- echo "PostgreSQL is up!"
+
+echo "PostgreSQL is up!"
+npm run prisma-push
+
+echo "⏳ Waiting for DynamoDB..."
+bash /wait-for-it.sh $DYNAMODB_HOST:$DYNAMODB_PORT --timeout=30 --strict -- echo "DynamoDB is up!"
+
+echo "DynamoDB is up!"
+npm run create-dDb-table
+
+npm run start
