43c883c17e0cbd99ad862105a91c14669d4d1db1
Hugo Manzoni <hugocmdesign@gmail.com>
2025-05-18 16:55:38 +0200

    remove redundant success messages in entrypoint scripts

--- docker/app/entrypoint.app.sh
+++ docker/app/entrypoint.app.sh
@@ -3,7 +3,6 @@ set -e
 
 echo "⏳ Waiting for PostgreSQL in $POSTGRES_HOST:$POSTGRES_PORT"
 wait-for-it.sh "$POSTGRES_HOST:$POSTGRES_PORT" --timeout=30 --strict -- echo "PostgreSQL is up!"
-echo "PostgreSQL is up!"
 
 sleep 10
 
@@ -11,7 +10,6 @@ npm run "prisma-push"
 
 echo "⏳ Waiting for DynamoDB in $DYNAMODB_HOST:$DYNAMODB_PORT"
 wait-for-it.sh "$DYNAMODB_HOST:$DYNAMODB_PORT" --timeout=30 --strict -- echo "DynamoDB is up!"
-echo "DynamoDB is up!"
 
 sleep 10
 
--- docker/app/entrypoint.consumer.sh
+++ docker/app/entrypoint.consumer.sh
@@ -3,7 +3,6 @@ set -e
 
 echo "⏳ Waiting for PostgreSQL in $POSTGRES_HOST:$POSTGRES_PORT"
 wait-for-it.sh "$POSTGRES_HOST:$POSTGRES_PORT" --timeout=30 --strict -- echo "PostgreSQL is up!"
-echo "PostgreSQL is up!"
 
 sleep 10
 
@@ -11,7 +10,6 @@ npm run "prisma-push"
 
 echo "⏳ Waiting for Kafka in $KAFKA_HOST:$KAFKA_PORT"
 wait-for-it.sh "$KAFKA_HOST:$KAFKA_PORT" --timeout=30 --strict -- echo "Kafka is up!"
-echo "Kafka is up!"
 
 sleep 10
 
