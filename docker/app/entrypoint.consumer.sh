14ed1b5101cd00b59ee6695b320bb49d26d97374
Hugo Manzoni <hugocmdesign@gmail.com>
2025-08-10 17:54:02 +0200

    refactor: update Dockerfiles and entrypoint scripts for improved dependency and error management

--- docker/app/entrypoint.consumer.sh
+++ docker/app/entrypoint.consumer.sh
@@ -4,14 +4,15 @@ set -e
 echo "⏳ Waiting for PostgreSQL in $POSTGRES_HOST:$POSTGRES_PORT"
 wait-for-it.sh "$POSTGRES_HOST:$POSTGRES_PORT" --timeout=30 --strict -- echo "PostgreSQL is up!"
 
-sleep 10
-
-npm run "prisma-push"
+echo "🚀 Pushing schema to PostgreSQL..."
+until npm run "prisma-push"; do
+  echo "prisma-push failed, retrying in 5 seconds..."
+  sleep 5
+done
+echo "✅ Schema pushed successfully."
 
 echo "⏳ Waiting for Kafka in $KAFKA_HOST:$KAFKA_PORT"
 wait-for-it.sh "$KAFKA_HOST:$KAFKA_PORT" --timeout=30 --strict -- echo "Kafka is up!"
-
-sleep 10
 
 echo "Starting app: $@"
 exec "$@"
